7abd1857ce4826770bbbd3ff35b01bea7b4f0ece
Standalone G.A.S <coder2k@mail.ru>
2021-01-02 16:08:32 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <ListItem v-for="res in storedResources"
+              :key="res.id"
+              :id="res.id"
+              :title="res.title"
+              :description="res.description"
+              :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import ListItem from "@/components/ListItem/ListItem";
+
+export default {
+  components: {
+    ListItem
+  },
+  name: "App",
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
